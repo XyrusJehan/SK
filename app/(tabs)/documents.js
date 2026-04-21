877cbabc2ca7fe074aa5f64e4e71d63802660018
Lozañes, Xyrus Jehan B <lozanessjehan@gmail.com>
2026-04-22 00:36:02 +0800

    Sidebar color

--- app/(tabs)/documents.js
+++ app/(tabs)/documents.js
@@ -449,13 +449,15 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     marginBottom: 8,
     alignItems: 'center',
-    backgroundColor: 'transparent',
+    borderWidth: 1.5,
+    borderColor: COLORS.black,
+    backgroundColor: '#133E75',
   },
-  navItemActive: {    backgroundColor: '#133E75',
+  navItemActive: {    backgroundColor: '#ffffff',
     borderWidth: 1.5,
     borderColor: COLORS.black, },
-  navLabel: { fontSize: 13, fontWeight: '600', color: 'rgba(255,255,255,0.7)', letterSpacing: 0.3 },
-  navLabelActive: { color: COLORS.white, fontWeight: '800' },
+  navLabel: { fontSize: 13, fontWeight: '600', color: 'rgba(0, 0, 0, 0.7)', letterSpacing: 0.3 },
+  navLabelActive: { color: COLORS.black, fontWeight: '800' },
 
   // Main
   main: {
