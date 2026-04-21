e4229b7bc5a9447f553fbfc6daef28acc9590867
Lozañes, Xyrus Jehan B <lozanessjehan@gmail.com>
2026-04-21 23:17:32 +0800

    a

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,15 +5,11 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-<<<<<<< HEAD
 import { NavProvider } from './navContext';
-=======
->>>>>>> 4883f35edbfdaedde52ffa8c6d7b191a5a3f2f24
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
-<<<<<<< HEAD
   return (
     <NavProvider>
       <Tabs
@@ -26,16 +22,3 @@ export default function TabLayout() {
     </NavProvider>
   );
 }
-=======
-  return ( 
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-<Tabs screenOptions={{ tabBarStyle: { display: "none" } }} />
-    </Tabs>
-  );
-}
->>>>>>> 4883f35edbfdaedde52ffa8c6d7b191a5a3f2f24
