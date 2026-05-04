e0e3ef426c9cb73d4b86d90eeacb202910ecf950
Lozañes, Xyrus Jehan B <lozanessjehan@gmail.com>
2026-05-04 21:18:49 +0800

    _layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,10 +7,11 @@ export default function TabLayout() {
   return (
     <NavProvider>
       <Tabs screenOptions={{ headerShown: false, tabBarStyle: { display: 'none' } }}>
-        <Tabs.Screen name="sk-home" />
+        <Tabs.Screen name="sk-dashboard" />
         <Tabs.Screen name="sk-document" />
         <Tabs.Screen name="sk-document-list" />
         <Tabs.Screen name="sk-planning" />
+        <Tabs.Screen name="sk-planning-templates" />
         <Tabs.Screen name="sk-portal" />
         <Tabs.Screen name="lydo-home" />
         <Tabs.Screen name="lydo-document" />
